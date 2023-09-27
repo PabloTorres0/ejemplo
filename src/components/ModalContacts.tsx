f9a3d6d2c73374a268b0a221b36f402f1efe5436
Pablo Torres <pablo.javier.torres.martinez@gmail.com>
2023-09-26 20:18:26 -0600

    Se agregó todo lo que se tenía que agregar

--- src/components/ModalContacts.tsx
+++ src/components/ModalContacts.tsx
@@ -1,136 +1,192 @@
-"use client"
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { db } from '../firebase'
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import { collection, addDoc, setDoc, doc } from 'firebase/firestore'
+import Alert from 'react-bootstrap/Alert'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-expressions
+// 'use client'
 interface Props {
-  modeModal: boolean;
-  modalOn: boolean;
-  data:{
-    name: string;
-    number: string;
-    id: string;
-    email: string;
+  modeModal: boolean
+  modalOn: boolean
+  data: {
+    name: string
+    number: string
+    id: string
+    email: string
   }
-  modalShow: (state:boolean)=>void;
+  modalShow: (state: boolean) => void
 }
 
-import {db} from "../firebase"
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
-import { collection, addDoc, setDoc, doc } from "firebase/firestore";
+const ModalContacts: React.FC<Props> = (props) => {
+  const [name, setName] = React.useState('')
+  const [email, setEmail] = React.useState('')
+  const [number, setNumber] = React.useState('')
+  const [textButton, setTextButton] = React.useState('')
+  const [emailValidation, setEmailValidation] = React.useState(false)
+  const [nameValidation, setNameValidation] = React.useState(false)
+  const [numberValidation, setNumberValidation] = React.useState(false)
 
-const ModalContacts = (props:Props) => {
-  
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [number, setNumber] = React.useState("");
-    const [textButton, setTextButton] = React.useState("")
+  const getName = (name: string): void => {
+    setName(name)
+    const regex = /^[a-zñ]+[a-zñ\s]+[a-zñ]$/
+    setNameValidation(regex.test(name.toLowerCase()))
+  }
+  const getEmail = (email: string): void => {
+    const regex =
+      // eslint-disable-next-line no-useless-escape
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    setEmailValidation(regex.test(email))
 
- 
-    const getName = (name:string) =>setName(name);
-    const getEmail = (email:string) =>setEmail(email);
-    const getNumber = (number:string) =>setNumber(number);
-    const setData=async ()=>{
-      console.log("Enviar datos")
-      props.modalShow(false)
+    setEmail(email)
+  }
+  const getNumber = (number: string): void => {
+    setNumber(number)
+    const regex = /^\d{10}$/
+    setNumberValidation(regex.test(number))
+  }
+  const setData = async (): Promise<void> => {
+    resetValidations()
+    console.log('Enviar datos')
+    props.modalShow(false)
 
-      
-      try {
-        if (props.modeModal===false){
-          const docRef = await addDoc(collection(db, "contacts"), {
-            name: name,
-            email: email,
-            number: number
-          });
-          console.log("Document written with ID: ", docRef.id);
-        }else{
-          await setDoc(doc(db, "contacts", props.data.id), {
-            name: name,
-            email: email,
-            number: number
-          });
-          console.log("Document written with ID: ", props.data.id);
-        }
+    try {
+      if (!props.modeModal) {
+        const docRef = await addDoc(collection(db, 'contacts'), {
+          name,
+          email,
+          number
+        })
+        console.log('Document written with ID: ', docRef.id)
+      } else {
+        await setDoc(doc(db, 'contacts', props.data.id), {
+          name,
+          email,
+          number
+        })
+        console.log('Document written with ID: ', props.data.id)
+      }
     } catch (e) {
-      console.error("Error adding document: ", e);
+      console.error('Error adding document: ', e)
     }
   }
-React.useEffect(()=>{
-  if (props.modeModal===false) {
-    setTextButton("AGREGAR CONTACTO")
-    setName("")
-    setNumber("")
-    setEmail("")
-  } else {
-    setTextButton("EDITAR CONTACTO") 
-    setName(props.data.name)
-    setNumber(props.data.number)
-    setEmail(props.data.email)
+  React.useEffect(() => {
+    if (!props.modeModal) {
+      setTextButton('AGREGAR CONTACTO')
+      setName('')
+      setNumber('')
+      setEmail('')
+      resetValidations()
+    } else {
+      setTextButton('EDITAR CONTACTO')
+      setName(props.data.name)
+      setNumber(props.data.number)
+      setEmail(props.data.email)
+
+      setEmailValidation(true)
+      setNumberValidation(true)
+      setNameValidation(true)
+    }
+    console.log(props.modeModal)
+
+    props.modalShow(props.modalOn)
+  }, [props.modeModal, props.modalOn])
+
+  const resetValidations = (): void => {
+    setEmailValidation(false)
+    setNumberValidation(false)
+    setNameValidation(false)
   }
-  console.log(props.modeModal)
-
-  props.modalShow(props.modalOn)
-},[props.modeModal,props.modalOn])
-
-
 
   return (
     <>
-
-      <Modal show={props.modalOn} onHide={()=>props.modalShow(false)}>
+      <Modal
+        show={props.modalOn}
+        onHide={() => {
+          props.modalShow(false)
+          resetValidations()
+        }}
+      >
         <Modal.Header closeButton>
-          <Modal.Title>{textButton  }</Modal.Title>
+          <Modal.Title>{textButton}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-            <>
-          <InputGroup size="sm" className="mb-2">
-            <InputGroup.Text id="inputGroup-sizing-sm">Nombre</InputGroup.Text>
-            <Form.Control
-            onChange={(e)=>{getName(e.target.value)}}
-              aria-label="Small" 
-              aria-describedby="inputGroup-sizing-sm"
-              value={name}
-            />
-          </InputGroup>
-          <br />
-          <InputGroup size="sm" className="mb-2">
-            <InputGroup.Text id="inputGroup-sizing-default">
-              Correo
-            </InputGroup.Text>
-            <Form.Control
-            onChange={(e)=>{getEmail(e.target.value)}}
-              aria-label="Default"
-              aria-describedby="inputGroup-sizing-default"
-              value={email}
-            />
-          </InputGroup>
-          <br />
-          <InputGroup size="sm">
-            <InputGroup.Text id="inputGroup-sizing-lg">Telefono</InputGroup.Text>
-            <Form.Control
-            onChange={(e)=>{getNumber(e.target.value)}}
-              aria-label="Large"
-              aria-describedby="inputGroup-sizing-sm"
-              value={number}
-            />
-          </InputGroup>
-        </>
-
-
-
+          <>
+            <InputGroup size="sm" className="mb-2">
+              <InputGroup.Text id="inputGroup-sizing-sm">
+                Nombre
+              </InputGroup.Text>
+              <Form.Control
+                onChange={(e) => {
+                  getName(e.target.value)
+                }}
+                aria-label="Small"
+                aria-describedby="inputGroup-sizing-sm"
+                value={name}
+              />
+            </InputGroup>
+            {!nameValidation && (
+              <Alert variant="danger">Ingresa Nombre Válido</Alert>
+            )}
+            <br />
+            <InputGroup size="sm" className="mb-2">
+              <InputGroup.Text id="inputGroup-sizing-default">
+                Correo
+              </InputGroup.Text>
+              <Form.Control
+                onChange={(e) => {
+                  getEmail(e.target.value)
+                }}
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                value={email}
+              />
+            </InputGroup>
+            {!emailValidation && (
+              <Alert variant="danger">Ingresa Email Válido</Alert>
+            )}
+            <br />
+            <InputGroup size="sm">
+              <InputGroup.Text id="inputGroup-sizing-lg">
+                Telefono
+              </InputGroup.Text>
+              <Form.Control
+                onChange={(e) => {
+                  getNumber(e.target.value)
+                }}
+                aria-label="Large"
+                aria-describedby="inputGroup-sizing-sm"
+                value={number}
+              />
+            </InputGroup>
+            {!numberValidation && (
+              <Alert variant="danger">Ingresa Teléfono Válido</Alert>
+            )}
+          </>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={()=>props.modalShow(false)}>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              props.modalShow(false)
+              resetValidations()
+            }}
+          >
             Cerrar
           </Button>
-          <Button variant="primary" onClick={setData}>
+          <Button
+            variant="primary"
+            onClick={setData}
+            disabled={!(emailValidation && nameValidation && numberValidation)}
+          >
             {textButton}
           </Button>
         </Modal.Footer>
       </Modal>
-
-      
     </>
   )
 }
--- src/components/SpinnerComponent.tsx
+++ src/components/SpinnerComponent.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import Spinner from 'react-bootstrap/Spinner';
+import Spinner from 'react-bootstrap/Spinner'
 
 const SpinnerComponent = () => {
   return (
-    <div className='text-center'>
-        <Spinner 
-        variant='light'
-        animation="border" />
+    <div className="text-center">
+      <Spinner variant="light" animation="border" />
     </div>
   )
 }
